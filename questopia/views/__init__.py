43457ac0cff91a9a16b2b437bec004d099c1307c
Celina <csarona@umich.edu>
2024-04-14 03:48:42 -0400

    added portals

--- questopia/views/__init__.py
+++ questopia/views/__init__.py
@@ -4,4 +4,4 @@ from questopia.views.env import show_env
 from questopia.views.env import show_env_post
 from questopia.views.env import process_question
 from questopia.views.quiz import show_quiz
-from questopia.views.completion import show_results
+from questopia.views.completion import show_results, show_portal
--- questopia/views/completion.py
+++ questopia/views/completion.py
@@ -1,49 +1,42 @@
-"""
-quest index (main) view.
-
-URLs include:
-/
-"""
 import flask
 import questopia
 from questopia.views.quiz import correct_answers, questions
 
-score = 0.1 
+score = None  # Initialize score as a global variable
 
 @questopia.app.route('/completion', methods=['POST'])
 def show_results():
+    global score  # Declare score as a global variable within the function
     answers = []
     wrong_questions = {}
     for question in flask.request.form:
         answers.append(flask.request.form[question])
 
-    # maybe show what question the kid got wrong
     wrong = 0
     for i in range(len(correct_answers)):
         if correct_answers[i] != answers[i]:
-            wrong+=1
+            wrong += 1
             wrong_questions[questions[i]] = correct_answers[i]
     print(correct_answers)
     print(answers)
 
     correct = len(correct_answers) - wrong
 
-    # for response in answers:
-    #     for correct_ans in correct_answers:
-
     context = {"correct": correct, 
                 "total": len(correct_answers),
                 "incorrect": wrong_questions
             }
-
-    score = correct/len(correct_answers)
+    if len(correct_answers) == 0:
+        score = 0
+    else:
+        score = correct / len(answers)
     
     return flask.render_template("completion.html", **context)
 
-@questopia.app.route('/portal')
-def open_portal():
-    context = {}
-    if (score > 0.5):
-        return flask.render_template("open_portal.html", **context)
-    return flask.render_template("closed_portal.html", **context)
-
+@questopia.app.route('/portal',  methods=['GET'])
+def show_portal():
+    global score  # Declare score as a global variable within the function
+    print(score)
+    if score > 0.5:
+        return flask.render_template("open_portal.html")
+    return flask.render_template("closed_portal.html")
